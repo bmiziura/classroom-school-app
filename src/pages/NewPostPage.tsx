a30652888db974f61d4da412428f75eb292f89b6
goblicpl <bartosz.miziura@o2.pl>
2022-10-07 08:14:22 +0200

    New Post Page + Firestore Configuration

--- src/pages/NewPostPage.tsx
+++ src/pages/NewPostPage.tsx
@@ -1,12 +1,64 @@
+import { FormEvent, useState } from "react"
+
 import Header from "../components/Header"
 
+import { CgSpinner } from "react-icons/cg"
+
 const NewPostPage = () => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  const [isLoading, setLoading] = useState(false)
+
+  const handlePost = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
   return (
-    <>
+    <div>
       <Header />
 
-      <div>NewPostPage</div>
-    </>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <div className="w-full max-w-[500px] flex flex-col gap-4 items-center bg-white p-4 py-16">
+          <h1 className="text-3xl font-bold text-center">New Post</h1>
+
+          <form
+            onSubmit={handlePost}
+            className="w-full max-w-[300px] flex flex-col gap-4"
+          >
+            <label className="flex flex-col">
+              <span className="px-2 mb-1">Title</span>
+              <input
+                type="text"
+                placeholder="Post Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="border rounded-md border-gray-500 placeholder-gray-500 p-2"
+              />
+            </label>
+
+            <label className="flex flex-col">
+              <span className="px-2 mb-1">Content</span>
+              <textarea
+                placeholder="Post Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className="border rounded-md border-gray-500 placeholder-gray-500 p-2"
+              ></textarea>
+            </label>
+
+            <button
+              type="submit"
+              name="Submit Post"
+              className="flex items-center justify-center border bg-blue-500 text-white font-bold py-2 px-4 rounded-md disabled:bg-gray-500"
+              disabled={isLoading}
+            >
+              {isLoading ? <CgSpinner className="animate-spin" /> : "Submit"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
